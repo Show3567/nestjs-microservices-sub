2ee23ffe69a69a1c72e440a4213bc05b7fbe37b1
Yue Dong <ydong011@fiu.edu>
2023-06-10 17:48:18 -0500

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger();
+  const port = 3001;
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(port, () => {
+    logger.log(`Your server listening on Port ${port}...`);
+  });
 }
 bootstrap();
